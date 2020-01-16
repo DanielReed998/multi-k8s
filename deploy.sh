6c800ef96d2796b855b11fe21a92f6254314e4f7
Daniel Reed <dreed@xometry.com>
2020-01-16 16:51:56 -0500

    added deploy scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t danielreed998/multi-client:latest -t danielreed998/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t danielreed998/multi-server:latest -t danielreed998/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t danielreed998/multi-worker:latest -t danielreed998/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push danielreed998/multi-client:latest
+docker push danielreed998/multi-server:latest
+docker push danielreed998/multi-worker:latest
+
+docker push danielreed998/multi-client:$SHA
+docker push danielreed998/multi-server:$SHA
+docker push danielreed998/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/client-deployment client=danielreed998/multi-client:$SHA
+kubectl set image deployments/server-deployment server=danielreed998/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=danielreed998/multi-worker:$SHA
